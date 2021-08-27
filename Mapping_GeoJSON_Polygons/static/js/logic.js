b0930b4d8d597b2f35b413e7784b12e8a4f9f1e2
Brieona <brieona.turner@gmail.com>
2021-08-27 18:08:46 -0400

    adding files

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,88 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Light: light,
+  Dark: dark
+};
+
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [light]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/BrieonaT/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+ style: myStyle,
+  onEachFeature: function (feature,layer) {
+    layer.bindPopup("<h3> Airline" + feature.properties.airline + "</h3> <hr> <h3> Destination:" + 
+    feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Accessing the airport GeoJSON URL
+//let airportData = "https://raw.githubusercontent.com/BrieonaT/Mapping_Earthquakes/main/Major%20Airports/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+//d3.json(airportData).then(function(data) {
+//    console.log(data);
+//  // Creating a GeoJSON layer with the retrieved data.
+//  L.geoJson(data, {
+//    onEachFeature: function(feature, layer) {
+//      layer.bindPopup("<h2>" + data.features[0].properties.alt + "</h2>");
+//     }
+//});
+//});
